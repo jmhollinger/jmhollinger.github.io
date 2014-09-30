59c2a4a7d563f4978594d5a18594fdec7c4bd9e8
jmhollinger <jmholl5@gmail.com>
2014-09-30 15:36:22 -0400

    Add Download Link

--- datatables/js/LoadTables.js
+++ datatables/js/LoadTables.js
@@ -7,7 +7,7 @@ function selectdata(dataset)
         {
           if (dataset === 'permits')
             {
-              $('#tablearea').html('<h3>Building Permits</h3><p>Below is a table of building permit issued by the City of Lexington in 2014. The table can be sorted by field or searched from the bar in the top right corner.</p><table class="hover" id="example"><thead><tr><th>Date</th><th>Address</th><th>Suite</th><th>Permit Type</th><th>Construction Cost</th><th>Owner</th><th>Contractor</th></tr></thead></table>')
+              $('#tablearea').html('<h3>Building Permits</h3><p>Below is a table of building permit issued by the City of Lexington in 2014. The table can be sorted by field or searched from the bar in the top right corner.</p><p><a href="2014/permits.csv">Download the Data</a></p><table class="hover" id="example"><thead><tr><th>Date</th><th>Address</th><th>Suite</th><th>Permit Type</th><th>Construction Cost</th><th>Owner</th><th>Contractor</th></tr></thead></table>')
               $('#example').dataTable( {
                  "order": [ 0, 'desc' ],
                  "deferRender": true,
@@ -26,7 +26,7 @@ function selectdata(dataset)
             }
           else if (dataset === 'code') 
             {
-              $('#tablearea').html('<h3>Code Enforcement Cases</h3><p>Below is a table Code Enforcement cases and opened by the City of Lexington in 2014. The table can be sorted by field or searched from the bar in the top right corner.</p><table class="hover" id="example"><thead><tr><th>Date Opened</th><th>Address</th><th>Case Type</th><th>Open/Closed</th><th>Status</th><th>Status Date</th></tr></thead></table>')
+              $('#tablearea').html('<h3>Code Enforcement Cases</h3><p>Below is a table Code Enforcement cases and opened by the City of Lexington in 2014. The table can be sorted by field or searched from the bar in the top right corner.</p><p><a href="2014/code.csv">Download the Data</a></p><table class="hover" id="example"><thead><tr><th>Date Opened</th><th>Address</th><th>Case Type</th><th>Open/Closed</th><th>Status</th><th>Status Date</th></tr></thead></table>')
                 $('#example').dataTable( {
                  "order": [ 0, 'desc' ],
                  "deferRender": true,
