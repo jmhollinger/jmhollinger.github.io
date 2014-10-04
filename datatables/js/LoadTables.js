f2bd23187d0da877ee00ede3abd81cd4a7411d2f
jmhollinger <jmholl5@gmail.com>
2014-10-04 17:16:10 -0400

    Refactor LoadTables.js , Update Index

--- datatables/js/LoadTables.js
+++ datatables/js/LoadTables.js
@@ -19,126 +19,59 @@
  * For details please refer to: http://www.datatables.net
 */
 
-/*the selectdata() function accepts the 'dataset' argument from HTML Buttons or other events inr order to display the datatable for each dataset*/
+/*the selectdata() function accepts the 'dataset' argument from HTML Buttons or other events to display the datatable for each dataset*/
 
 function selectdata(dataset)
         {
-          /*Create Divs*/
-        $('#content').html('<div id="headerarea"></div><div id="metaarea"></div><div id="tablearea"></div>')
-
-/*Begin Permits Datatable*/
-            if (dataset === 'permits')
-            {
-              var title = 'Building Permits'
-              var url = 'permits'
-              var coltitles = ["Date", "Address", "Suite","Type","Construction Cost","Owner","Contractor"]
-              var metadata = 'Building permits are issued by the Division of Building Inspection for a variety of construction activity, including residential and commercial building construction, remodeling, decks, fences, HVAC, and many other categories. The construction cost of projects is provided by the applicant.'
+/*Create Variables*/
+        var title = ""
+        var jsonurl = ""
+        var csvurl = ""
+        var metadata = ""
+        var coltitles = ""
+        var datatitles = ""
+/*Permits Variable assignment*/       
+        if (dataset === 'permits') {
+            title = 'Building Permits'
+            jsonurl = 'data/permits.json'
+            csvurl = 'data/permits.csv'
+            metadata = 'Building permits are issued by the Division of Building Inspection for a variety of construction activity, including residential and commercial building construction, remodeling, decks, fences, HVAC, and many other categories. The construction cost of projects is provided by the applicant.'
+            coltitles = ["Date", "Address", "Suite","Type","Construction Cost","Owner","Contractor"]
+            datatitles = [{ "data": "Date_1"},{ "data": "Address_1"},{ "data":  "Suite_1"},{ "data":  "Type"},{ "data":  "ConstructionCost_1"},{ "data":  "Owner_1"},{ "data":  "Contractor_1"}]
+        }
+/*Code Variable assignment*/
+        else if (dataset === 'code') {
+            title = 'Code Enforcement Cases'
+            jsonurl = 'data/code.json'
+            csvurl = 'data/code.csv'
+            metadata = 'The Division of Code Enforcement inspects and cites nuisance, housing, and sidewalk violations as categorized under the LFUCG Code of Ordinances, and the International Property Maintenance Code.'
+            coltitles = ["Date Opened", "Address", "Case Type","Open/Closed","Status","Status Date"]
+            datatitles = [{ "data": "DateOpened_1" },{ "data":  "Address_1" },{ "data":  "CaseType" },{ "data": "OpenClosed" },{ "data": "Status" },{ "data": "StatusDate_1"}]
+        }
+/*ROW Variable assignment*/
+        else if (dataset === 'row') {
+            title = 'Right-of-Way Permits'
+            jsonurl = 'data/row.json'
+            csvurl = 'data/row.csv'
+            metadata = 'Any entity performing work on a public street or within the right-of-way is required to have a right-of-way permit.'
+            coltitles = ["Date", "Address", "Permit Type","Type of Work","Entity Performing Work","Start Date","End Date","Lane Blockage"]
+            datatitles = [{ "data": "date" },{ "data": "address" },{ "data": "type" },{ "data": "work" },{ "data": "entity" },{ "data": "start" },{ "data": "end" },{ "data": "lnblock" }]
+        }
+        else {
+            $('#content').html('<h3>Planning, Preservation, and Development Data Browser</h3><p>Please select a topic from the navigation bar to begin exploring city data.</p>')
+        }
+/*Create Descriptive Content*/
+              $('#content').html('<div id="headerarea"></div><div id="metaarea"></div><div id="tablearea"></div>')
               $('#headerarea').html('<h3>'+ title +'</h3>')
-              $('#tablearea').html('<table id="example" class ="hover"><thead><tr id="hdrow"></tr></thead></table>')
-              $.each(coltitles, function( index, value ) {$("#hdrow").append('<th>'+ value +'</th>')});
-              $('#metaarea').html('<p>' + metadata + '</p><a href="data/' + url +'.csv">Download the Data</a><p></p>')
-              $('#example').dataTable( {
+              $('#metaarea').html('<p>' + metadata + '</p><a href="' + csvurl +'">Download the Data</a><p></p>')
+              $('#tablearea').html('<table id="databrowser" class ="hover"><thead><tr id="hdrow"></tr></thead></table>')
+              $.each(coltitles, function( index, value ) {$("#hdrow").append('<th>'+ value +'</th>')})
+/*Create Table*/
+              $('#databrowser').dataTable( {
                  "order": [ 0, 'desc' ],
                  "deferRender": true,
                  "destroy" : true, 
-                 "ajax": "data/" + url + ".json",
-                 "columns": [
-                     { "data": "Date_1" },
-                     { "data": "Address_1" },
-                     { "data": "Suite_1" },
-                     { "data": "Type" },
-                     { "data": "ConstructionCost_1" },
-                     { "data": "Owner_1" },
-                     { "data": "Contractor_1" }
-                     ]
-                                  } )
-            }
-/*End Permits Datatable*/
-
-/*Begin Code Datatable*/
-          else if (dataset === 'code') 
-            {
-              var title = 'Code Enforcement Cases'
-              var url = 'code'
-              var coltitles = ["Date Opened", "Address", "Case Type","Open/Closed","Status","Status Date"]
-              var metadata = 'The Division of Code Enforcement inspects and cites nuisance, housing, and sidewalk violations as categorized under the LFUCG Code of Ordinances, and the International Property Maintenance Code.'
-              $('#headerarea').html('<h3>'+ title +'</h3>')
-              $('#tablearea').html('<table id="example" class ="hover"><thead><tr id="hdrow"></tr></thead></table>')
-              $.each(coltitles, function( index, value ) {$("#hdrow").append('<th>'+ value +'</th>')});
-              $('#metaarea').html('<p>' + metadata + '</p><a href="data/' + url +'.csv">Download the Data</a><p></p>')
-              $('#example').dataTable( {
-                 "order": [ 0, 'desc' ],
-                 "deferRender": true,
-                 "destroy" : true, 
-                 "ajax": "data/" + url + ".json",
-                 "columns": [
-                     { "data": "DateOpened_1" },
-                     { "data": "Address_1" },
-                     { "data": "CaseType" },
-                     { "data": "OpenClosed" },
-                     { "data": "Status" },
-                     { "data": "StatusDate_1" }
-                     ]
-                                 } )
-            }
-/*End Code Datatable*/
-
-/*Begin ROW Datatable*/
-            else if (dataset === 'row') 
-             {
-              var title = 'Right-of-Way Permits'
-              var url = 'row'
-              var coltitles = ["Date", "Address", "Permit Type","Type of Work","Entity Performing Work","Start Date","End Date","Lane Blockage"]
-              var metadata = 'Any entity performing work in a public street or within the right-of-wayis required to acquire a right-of-way permit. Some work is allowed under an "Annual General Permit" while other requires specific permits for the activity.'
-              $('#headerarea').html('<h3>'+ title +'</h3>')
-              $('#tablearea').html('<table id="example" class ="hover"><thead><tr id="hdrow"></tr></thead></table>')
-              $.each(coltitles, function( index, value ) {$("#hdrow").append('<th>'+ value +'</th>')});
-              $('#metaarea').html('<p>' + metadata + '</p><a href="data/' + url +'.csv">Download the Data</a><p></p>')
-              $('#example').dataTable( {
-                 "order": [ 0, 'desc' ],
-                 "deferRender": true,
-                 "destroy" : true, 
-                 "ajax": "data/" + url + ".json",
-                 "columns": [
-                     { "data": "date" },
-                     { "data": "address" },
-                     { "data": "type" },
-                     { "data": "work" },
-                     { "data": "entity" },
-                     { "data": "start" },
-                     { "data": "end" },
-                     { "data": "lnblock" },
-                     ]
+                 "ajax": jsonurl,
+                 "columns": datatitles
                                 } )
-            }
-/*End ROW Datatable*/
-
-/*Begin Historic Preservation Datatable*/
-            else if (dataset === 'hp') 
-             {
-              var title = 'Historic Preservation'
-              var url = 'hp'
-              var coltitles = ["Date", "Address", "Permit Type","Type of Work","Entity Performing Work","Start Date","End Date","Lane Blockage"]
-              var metadata = 'This is the metadata for this dataset'
-              $('#headerarea').html('<h3>'+ title +'</h3>')
-              $('#tablearea').html('')
-              $.each(coltitles, function( index, value ) {$("#hdrow").append('<th>'+ value +'</th>')});
-              $('#metaarea').html('<p>' + metadata + '</p><a href="data/' + url +'.csv">Download the Data</a><p></p>')
-              $('#example').dataTable( {
-                 "order": [ 0, 'desc' ],
-                 "deferRender": true,
-                 "destroy" : true, 
-                 "ajax": "data/" + url + ".json",
-                 "columns": [
-                     { "data": "date" },
-                     { "data": "address" },
-                     { "data": "type" },
-                     { "data": "work" },
-                     { "data": "entity" },
-                     { "data": "start" },
-                     { "data": "end" },
-                     { "data": "lnblock" },
-                     ]
-                                } )
-            }
-        };
+        }
