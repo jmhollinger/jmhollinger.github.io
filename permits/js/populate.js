0037e0f67928351e6ce86970df5fe2b6d32d8b03
jmhollinger <jmholl5@gmail.com>
2014-10-30 12:01:21 -0400

    Add Data Type Parameter

--- permits/js/populate.js
+++ permits/js/populate.js
@@ -2,11 +2,13 @@
 
 
 $(document).ready(populate);
-    function populate()
+
+function populate()
     {
-    var permitnum = getUrlParameter('ID')
+    var dataset = getUrlParameter('Type')
+    var idnum = getUrlParameter('ID')
     var params = {
-        sql: 'SELECT "ID", "Date","Address", "Suite","PermitType", "ConstructionCost","OwnerName","Contractor","lat","lng" FROM "2691aff1-e555-48d3-9188-aebf1fa8323e" WHERE "ID" = ' + permitnum};
+        sql: 'SELECT "ID", "Date","Address", "Suite","PermitType", "ConstructionCost","OwnerName","Contractor","lat","lng" FROM "2691aff1-e555-48d3-9188-aebf1fa8323e" WHERE "ID" = ' + idnum};
     $.ajax({
         url: '//www.civicdata.com/api/action/datastore_search_sql',
         data: params,
@@ -18,12 +20,13 @@ $(document).ready(populate);
         
         $("#map").html('<iframe width="500" height="500" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=' + property.Address + ' Lexington KY United States &key=AIzaSyDXqhUx3ZQwPBtAVsXg6tz9N_2yvrRydcQ"></iframe>')
 
-        $("#contact").html('<p>If you have questions or concerns about this permit please contact the Division of Building Inspection at (859) 258-3770 or by email at <a  href = "#" >buildinginspection@lexingtonky.gov</a></p>')
+        $("#contact").html('<p>If you have questions or concerns about this ' + dataset + ' please contact the Division of Building Inspection at (859) 258-3770 or by email at <a  href = "#" >buildinginspection@lexingtonky.gov</a></p>')
                  
         });
         }
       })} 
-          function getUrlParameter(sParam)
+    
+    function getUrlParameter(sParam)
     {
     var sPageURL = window.location.search.substring(1);
     var sURLVariables = sPageURL.split('&');
