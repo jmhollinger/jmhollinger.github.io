e630fbb335c641785128397c405a8a4a29888348
jmhollinger <jmholl5@gmail.com>
2014-10-30 11:47:46 -0400

    Split into files

---
+++ permits/js/populate.js
@@ -0,0 +1,38 @@
+/*Created by Jonathan Hollinger, use at your own risk. */
+
+
+$(document).ready(populate);
+    function populate()
+    {
+    var permitnum = getUrlParameter('ID')
+    var params = {
+        sql: 'SELECT "ID", "Date","Address", "Suite","PermitType", "ConstructionCost","OwnerName","Contractor","lat","lng" FROM "2691aff1-e555-48d3-9188-aebf1fa8323e" WHERE "ID" = ' + permitnum};
+    $.ajax({
+        url: '//www.civicdata.com/api/action/datastore_search_sql',
+        data: params,
+        dataType: 'jsonp',
+        success: function(data) {
+        $.each(data.result.records, function(key, property){
+        
+        $("#details").html('<h3>Building Permit</h3><ul class="permit"><li><b>Permit ID:</b> ' + property.ID + '</li><li><b>Date:</b> ' + property.Date + '</li><li><b>Address:</b> ' + property.Address + ' ' + property.Suite + '</li>  <li><b>Permit Type:</b> ' + property.PermitType + '</li><li><b>Construction Cost:</b> $' + property.ConstructionCost + '</li>  <li><b>Owner:</b> ' + property.OwnerName + '</li><li><b>Contractor:</b> ' + property.Contractor + '</li></ul>')    
+        
+        $("#map").html('<iframe width="500" height="500" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=' + property.Address + ' Lexington KY United States &key=AIzaSyDXqhUx3ZQwPBtAVsXg6tz9N_2yvrRydcQ"></iframe>')
+
+        $("#contact").html('<p>If you have questions or concerns about this permit please contact the Division of Building Inspection at (859) 258-3770 or by email at <a  href = "#" >buildinginspection@lexingtonky.gov</a></p>')
+                 
+        });
+        }
+      })} 
+          function getUrlParameter(sParam)
+    {
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) 
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) 
+        {
+            return sParameterName[1];
+        }
+    }
+} 
