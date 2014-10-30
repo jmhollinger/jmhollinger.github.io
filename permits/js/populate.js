6bf530543a0421fb14167a730e9a397d71488880
jmhollinger <jmholl5@gmail.com>
2014-10-30 16:26:33 -0400

    Add Code Query

--- permits/js/populate.js
+++ permits/js/populate.js
@@ -9,8 +9,10 @@ function populate()
     var idnum = getUrlParameter('ID')
 
     if (dataset == 'permit') { 
+      var fields = '"ID", "Date","Address", "Suite","PermitType", "ConstructionCost","OwnerName","Contractor","lat","lng"'
+      var resource = '2691aff1-e555-48d3-9188-aebf1fa8323e'
       var params = {
-          sql: 'SELECT "ID", "Date","Address", "Suite","PermitType", "ConstructionCost","OwnerName","Contractor","lat","lng" FROM "2691aff1-e555-48d3-9188-aebf1fa8323e" WHERE "ID" = ' + idnum};
+          sql: 'SELECT' + fields + 'FROM "' + resource + '" WHERE "ID" = ' + idnum};
       $.ajax({
           url: '//www.civicdata.com/api/action/datastore_search_sql',
           data: params,
@@ -22,14 +24,16 @@ function populate()
           
           $("#map").html('<iframe width="500" height="500" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=' + property.Address + ' Lexington KY United States &key=AIzaSyDXqhUx3ZQwPBtAVsXg6tz9N_2yvrRydcQ"></iframe>')
 
-          $("#contact").html('<p>If you have questions or concerns about this ' + dataset.toTitleCase() + ' please contact the Division of Building Inspection at (859) 258-3770 or by email at <a  href = "#" >buildinginspection@lexingtonky.gov</a></p>')
+          $("#contact").html('<p>If you have questions or concerns about this building permit please contact the Division of Building Inspection at (859) 258-3770.</p>')
                    
           });
           }
         })} 
     else if (dataset == 'code') { 
+      var fields = '"CaseNo", "DateOpened","Address","CaseType", "Status","StatusDate", "Closed","lat","lng"'
+      var resource = 'ad346da7-ce88-4c77-a0e1-10ff09bb0622'
       var params = {
-          sql: 'SELECT "ID", "Date","Address", "Suite","PermitType", "ConstructionCost","OwnerName","Contractor","lat","lng" FROM "2691aff1-e555-48d3-9188-aebf1fa8323e" WHERE "ID" = ' + idnum};
+          sql: 'SELECT' + fields + 'FROM "' + resource + '" WHERE "ID" = ' + idnum};
       $.ajax({
           url: '//www.civicdata.com/api/action/datastore_search_sql',
           data: params,
@@ -37,11 +41,11 @@ function populate()
           success: function(data) {
           $.each(data.result.records, function(key, property){
           
-          $("#details").html('<h3>Code Enforcement Case</h3><ul class="permit"><li><b>Permit ID:</b> ' + property.ID + '</li><li><b>Date:</b> ' + property.Date + '</li><li><b>Address:</b> ' + property.Address.toTitleCase() + ' ' + property.Suite.toTitleCase() + '</li>  <li><b>Permit Type:</b> ' + property.PermitType.toTitleCase() + '</li><li><b>Construction Cost:</b> $' + CurrencyFormat(property.ConstructionCost) + '</li>  <li><b>Owner:</b> ' + property.OwnerName.toTitleCase() + '</li><li><b>Contractor:</b> ' + property.Contractor.toTitleCase() + '</li></ul>')    
+          $("#details").html('<h3>Building Permit</h3><ul class="permit"><li><b>Case No:</b> ' + property.CaseNo + '</li><li><b>Date Opened:</b> ' + property.DateOpened + '</li><li><b>Address:</b> ' + property.Address.toTitleCase() + '</li>  <li><b>Case Type:</b> ' + property.CaseType.toTitleCase() + '</li><li><b>Status:</b> $' + property.Status.toTitleCase() + '</li>  <li><b>Status Date:</b> ' + property.StatusDate + '</li></ul>')    
           
           $("#map").html('<iframe width="500" height="500" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=' + property.Address + ' Lexington KY United States &key=AIzaSyDXqhUx3ZQwPBtAVsXg6tz9N_2yvrRydcQ"></iframe>')
 
-          $("#contact").html('<p>If you have questions or concerns about this ' + dataset.toTitleCase() + ' please contact the Division of Building Inspection at (859) 258-3770 or by email at <a  href = "#" >buildinginspection@lexingtonky.gov</a></p>')
+          $("#contact").html('<p>If you have questions or concerns about this building permit please contact the Division of Building Inspection at (859) 258-3770.</p>')
                    
           });
           }
