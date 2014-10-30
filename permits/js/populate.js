933c08f9e029bbfd4772d42a86fed3750ef0fe5c
Jonathan Hollinger <jmholl5@gmail.com>
2014-10-30 19:29:57 -0400

    Update populate.js

--- permits/js/populate.js
+++ permits/js/populate.js
@@ -41,11 +41,11 @@ function populate()
           success: function(data) {
           $.each(data.result.records, function(key, property){
           
-          $("#details").html('<h3>Building Permit</h3><ul class="permit"><li><b>Case No:</b> ' + property.CaseNo + '</li><li><b>Date Opened:</b> ' + property.DateOpened + '</li><li><b>Address:</b> ' + property.Address.toTitleCase() + '</li>  <li><b>Case Type:</b> ' + property.CaseType.toTitleCase() + '</li><li><b>Status:</b> ' + property.Status.toTitleCase() + '</li>  <li><b>Status Date:</b> ' + property.StatusDate + '</li></ul>')    
+          $("#details").html('<h3>Code Enforcement Case</h3><ul class="permit"><li><b>Case No:</b> ' + property.CaseNo + '</li><li><b>Date Opened:</b> ' + property.DateOpened + '</li><li><b>Address:</b> ' + property.Address.toTitleCase() + '</li>  <li><b>Case Type:</b> ' + property.CaseType.toTitleCase() + '</li><li><b>Status:</b> ' + property.Status.toTitleCase() + '</li>  <li><b>Status Date:</b> ' + property.StatusDate + '</li></ul>')    
           
           $("#map").html('<iframe width="500" height="500" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=' + property.Address + ' Lexington KY United States &key=AIzaSyDXqhUx3ZQwPBtAVsXg6tz9N_2yvrRydcQ"></iframe>')
 
-          $("#contact").html('<p>If you have questions or concerns about this building permit please contact the Division of Building Inspection at (859) 258-3770.</p>')
+          $("#contact").html('<p>If you have questions or concerns about this Code Enforcement case please contact the Division of Code Enforcement at (859) 425-2255.</p>')
                    
           });
           }
