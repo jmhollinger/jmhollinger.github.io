b9c1a8f06336114003dde334b26217f2f0df3198
jmhollinger <jmholl5@gmail.com>
2014-12-04 13:24:19 -0500

    Update One Stop

--- onestop/js/onestop.js
+++ onestop/js/onestop.js
@@ -7,22 +7,22 @@ $(document).ready(function() {$('#appform').bootstrapValidator({
         },
         trigger: 'input change keyup',
         fields: {
-          name: {validators: {notEmpty: {message: 'Name of business cannot be empty'}}},
-          contact: {validators: {notEmpty: {message: 'Contact Person cannot be empty'}}},
-          phone1: {validators: {notEmpty: {message: 'Primary Phone cannot be empty'}, phone: {country: 'US', message: 'Primary phone is not a valid phone number, try something like 859-425-2255'}}},
+          name: {validators: {notEmpty: {message: 'Name of business cannot be left blank.'}}},
+          contact: {validators: {notEmpty: {message: 'Contact Person cannot be left blank'}}},
+          phone1: {validators: {notEmpty: {message: 'Primary Phone cannot be left blank'}, phone: {country: 'US', message: 'Primary phone is not a valid phone number, try something like 859-425-2255'}}},
           phone2: {validators: {phone: {country: 'US', message: 'Secondary phone is not a valid phone number, try something that looks like 859-425-2255'}}},
-          email: {validators: {notEmpty: {message: 'The email address cannot be empty'},emailAddress: {message: 'The email address is not a valid, try something that looks like email@lexintonky.gov'}}},
-          address: {validators: {notEmpty: {message: 'Address cannot be empty.'}}},
-          city: {validators: {notEmpty: {message: 'City cannot be empty.'}, regexp: {regexp: /^lexington$/i, message: 'The business location must be in Lexington.'}}},
-          state: {validators: {notEmpty: {message: 'State cannot be empty.'}, regexp: {regexp: /^ky$/i, message: 'The business location must be in KY.'}}},
-          zip: {validators: {notEmpty: {message: 'Zip cannot be empty.'}, regexp: {regexp: /^\d{5}$/, message: 'Zip code is not valid, try something that looks like 40508.'}}},
-          busaddress: {validators: {notEmpty: {message: 'Address cannot be empty.'}}},
-          buscity: {validators: {notEmpty: {message: 'City cannot be empty.'}}},
-          busstate: {validators: {notEmpty: {message: 'State cannot be empty.'}}},
-          bustype: {validators: {notEmpty: {message: 'Business type cannot be empty.'}}},
-          buszip: {validators: {notEmpty: {message: 'Zip cannot be empty.'}, regexp: {regexp: /^\d{5}$/, message: 'Zip code is not valid, try something that looks like 40508.'}}},
-          busactivities: {validators: {notEmpty: {message: 'Business activities cannot be empty.'}}},
-          verify: {validators: {notEmpty: {message: 'This box must be checked to submit the application.'}}}
+          email: {validators: {notEmpty: {message: 'The email address cannot be left blank'},emailAddress: {message: 'The email address is not a valid, try something that looks like email@lexintonky.gov'}}},
+          address: {validators: {notEmpty: {message: 'Address cannot be left blank.'}}},
+          city: {validators: {notEmpty: {message: 'City cannot be left blank.'}, regexp: {regexp: /^lexington$/i, message: 'The business location must be in Lexington.'}}},
+          state: {validators: {notEmpty: {message: 'State cannot be left blank.'}, regexp: {regexp: /^ky$/i, message: 'The business location must be in KY.'}}},
+          zip: {validators: {notEmpty: {message: 'Zip cannot be left blank.'}, regexp: {regexp: /^\d{5}$/, message: 'Zip code is not valid, try something that looks like 40508.'}}},
+          busaddress: {validators: {notEmpty: {message: 'Address cannot be left blank.'}}},
+          buscity: {validators: {notEmpty: {message: 'City cannot be left blank.'}}},
+          busstate: {validators: {notEmpty: {message: 'State cannot be left blank.'}}},
+          bustype: {validators: {notEmpty: {message: 'Business type cannot be left blank.'}}},
+          buszip: {validators: {notEmpty: {message: 'Zip cannot be left blank.'}, regexp: {regexp: /^\d{5}$/, message: 'Zip code is not valid, try something that looks like 40508.'}}},
+          busactivities: {validators: {notEmpty: {message: 'Business activities cannot be left blank.'}}},
+          verify: {validators: {notEmpty: {message: 'You must agree to the statement above to submit the application.'}}}
         }
         });
 });
