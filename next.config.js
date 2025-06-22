f29a9371cdc1ea9215c7710f5bf37209c0b9e4bf
Jonathan Hollinger <jonathanh@mangomint.com>
2025-06-22 08:52:23 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,9 +4,7 @@ const nextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true
-  },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/jmhollinger.github.io' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/jmhollinger.github.io' : '',
+  }
 }
 
 module.exports = nextConfig 
